9eb4a7a3285e59ca3e9eac1ec2354608d07a42c2
pathas <pathas@naver.com>
2023-04-13 00:22:16 +0900

    진동 테스트

--- src/utils/vibrate.ts
+++ src/utils/vibrate.ts
@@ -1,6 +1,6 @@
 export const vibrate = (ms = 20) => {
   //@ts-ignore
-  navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+  const vibrateFunc = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 
-  if (navigator.vibrate) navigator.vibrate(ms);
+  if (vibrateFunc) vibrateFunc(ms);
 };
